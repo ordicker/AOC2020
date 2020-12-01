e96633c8a1f3d606b166879bd416e4f5059d7549
Gili Weiss Dicker <gili@apple.com>
2020-12-01 21:20:25 +0200

    updated func.jl

--- 1/func.jl
+++ 1/func.jl
@@ -4,4 +4,11 @@ export sol1
 
 sol1(input) = 514579
 
+function play(input) # returns a sorted array with file_path elements 
+    num_arr = [parse(Int, input) for input in split(s)]
+    sort!(num_arr)
+    return num_arr
+end
+
+
 end
