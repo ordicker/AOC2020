031400d6bc064b651e57725e5a953bf1e1aca2a0
Or <or.dicker@gmail.com>
2020-12-01 21:41:14 +0200

    solution to part1

--- 1/func.jl
+++ 1/func.jl
@@ -5,9 +5,15 @@ export sol1
 function sol1(input)  
     num_arr = [parse(Int, ii) for ii in split(input)]
     sort!(num_arr)
-
-
-    return num_arr
+    for big in reverse(num_arr)
+        small = 2020 - big
+        ind = findfirst(num_arr.==small)
+        if(ind ≠ nothing)
+            return big*num_arr[ind]
+        end
+    end
+    println("We didn't find two numbers that summed to 2020")
+    return nothing
 end
 
 
