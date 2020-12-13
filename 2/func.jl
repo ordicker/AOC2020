b15bb79481630d20b85a4078ffbd77ac35763b50
Gili Weiss Dicker <gili@apple.com>
2020-12-13 16:16:13 +0200

    sum

--- 2/func.jl
+++ 2/func.jl
@@ -7,17 +7,17 @@ function sol1(input_path)
 	regex_name = r"^(\d*)-(\d*) (\w): (\w*)"
 
     for line in eachline(input_path)
-        #println(match(regex_name, line))
         reg_line = match(regex_name, line)
 
     	low_lim = parse(Int, reg_line[1]) 
     	high_lim = parse(Int, reg_line[2])
-    	element = reg_line[3]
+    	element = convert(Char,reg_line[3]) 
     	str = reg_line[4]
 
     	counter = count(x->x==element, str)
+        println(counter)
         if (counter >= low_lim && counter <= high_lim)
-            valids++    
+            valids = valids + 1    
         end
     end
     return valids
