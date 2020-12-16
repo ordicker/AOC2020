a0adbd262568aa0736febaf8fe6c710ce04f8e9d
Gili Weiss Dicker <gili@apple.com>
2020-12-16 11:19:50 +0200

    Update test.jl

--- 2/test.jl
+++ 2/test.jl
@@ -15,3 +15,14 @@ end
 
 myans = day2.sol1("input.txt")
 println("The answer is: $myans")
+
+print("test2: ")
+myans2 = day2.sol2("input1.txt")
+if(myans2 == ans2)
+    println("right!")
+else
+    println("wrong! your answer is $myans2, expected: $ans2")
+end
+
+myans = day2.sol2("input.txt")
+println("The answer is: $myans")
